03f7700c61114d7b8199156a04bd41e54692daed
zhengguang <zhuzhengguang@1yd.me>
2015-11-15 00:13:54 +0800

    update dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-__author__ = 'zzg'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-11-15 00:10:41
+# @Author  : zheng guang (zzglfh@gmail)
+# @Link    : 
+# @Version : $Id$
+
 import flaskr
 
 flaskr.app.logger.setLevel("DEBUG")
--- flaskr.py
+++ flaskr.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-11-15 00:10:41
+# @Author  : zheng guang (zzglfh@gmail)
+# @Link    : 
+# @Version : $Id$
+
 # all the imports
 import sqlite3
 import os
